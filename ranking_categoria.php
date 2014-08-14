134dab170f07951f05e9048fdf69dd41a9a7c922
augustocarvalho <augusto.unit@gmail.com>
2014-08-14 01:46:13 -0300

    Segunda Etapa Resultados

---
+++ ranking_categoria.php
@@ -0,0 +1,66 @@
+<?php
+require_once "menu.php";
+include 'config.php';
+
+    $id = 1;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+	
+	
+	$id_categoria = $_GET['idc'];
+	
+
+?>
+
+<html>
+<body>
+    <div class="container">
+            <div class="row">
+			       <?php
+				$categ = mysql_query(" select * from categoria where id_categoria='$id_categoria'");  
+                $etapa = mysql_query("select local from etapa where id_etapa='$id'");
+					while ($result_categ = mysql_fetch_assoc($categ)){	
+							while ($result = mysql_fetch_assoc($etapa)){
+							  echo '<br>';
+							  echo '<h3 align="center">' . $result['local'] .  '</h3>';
+							  echo '<h4 align="center">' . $result_categ['nome_categoria'] .  '</h4>';
+							 echo '<br>';
+							  echo '<br>';
+							}
+                    }
+              ?>
+             
+            </div>
+            <div class="row">
+                <table cellpadding="0" cellspacing="0" border="0"   style="width: 600px"  align="center" class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>NOME</th>
+                      <th>UF</th>
+					  <th>PONTOS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+						
+						$count=1;
+					    $sql = mysql_query("SELECT nome_participante, estado, pontos FROM ranking r join participante p  
+															WHERE r.id_categoria='$id_categoria' and r.id_participante=p.id_participante order by pontos DESC");
+                   	while ($row = mysql_fetch_assoc($sql) )  {
+                            echo '<tr>';
+                            echo '<td>' . $count . '</td>';
+                            echo '<td>'. $row['nome_participante'] . '</td>';
+                            echo '<td class="span1">'. $row['estado'] . '</td>';
+						              	echo '<td>'. $row['pontos'] . '</td>';
+                            echo '</tr>';
+                            $count++;
+                    }
+                   ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
+</html>

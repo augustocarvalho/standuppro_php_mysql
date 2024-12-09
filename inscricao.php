838ddb309fba7acfbf983284206723c2a3304b52
Augusto Carvalho <augusto.unit@gmail.com>
2024-12-09 20:22:26 +0100

    quinta etapa abasup 2024 - ranking

--- inscricao.php
+++ inscricao.php
@@ -287,7 +287,7 @@
             <label class="control-label">Etapa:</label>
               <div class="controls">
                 <select class="span4" name="etapa" >
-                    <option value="63">QUARTA ETAPA CBSUP 2024</option>
+                    <option value="64">QUINTA ETAPA ABASUP 2024</option>
                 </select> 
                </div>   
           </div>        
